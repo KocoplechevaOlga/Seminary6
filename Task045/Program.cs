dd7ebf01b0a954be18c4a6602e48d54401bce70e
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-14 12:16:52 +0300

    Task45

--- Task045/Program.cs
+++ Task045/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет создавать копию заданного массива 
+//с помощью поэлементного копирования.
+
+int[] FullArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int [] copyArr (int [] arr)
+{
+    int [] copArr  = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copArr[i] = arr [i];
+    }
+    return copArr;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int [] firstArr = (FullArray(5, 1, 11));
+PrintArray(firstArr);
+int [] secArr = copyArr(firstArr);
+PrintArray(secArr);
