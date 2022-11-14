5cfb5f6175d6dca25f5d23ad1c082425741ef35b
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-14 10:53:03 +0300

    Task40

--- Task040/Program.cs
+++ Task040/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника
+//меньше суммы двух других сторон.
+
+bool TeorSumTrisngl (int a, int b, int c)
+{
+    return c < a + b && a < b + c && b < a + c;
+}
+
+Console.WriteLine("Введите 1 сторону: ");
+int len1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 сторону: ");
+int len2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 3 сторону: ");
+int len3 = Convert.ToInt32(Console.ReadLine());
+
+bool res = TeorSumTrisngl(len1, len2, len3);
+Console.WriteLine(res == true ? "Треугольник возможен" : "Треугольник не возможен");
